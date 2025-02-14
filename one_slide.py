418e6554c17e702c67e1f22582e4b31194ab0f9b
Theo Beers <theo.beers@drexel.edu>
2025-02-14 10:04:40 -0500

    Add comments

--- one_slide.py
+++ one_slide.py
@@ -4,7 +4,20 @@ from datetime import datetime
 from d10n.basic import archive, convert, metadata_prep, setup
 from d10n.classes.digislide import DigiSlide
 
-# Need to install mamba, pyimagej, and then bioformats2raw, and raw2ometiff in pyimagej side of mamba
+# Notes from Theo on getting this to run:
+# - pyimagej installed as Python dep via uv (see pyproject.toml)
+# - Need Java installation (I chose openjdk-17-jre-headless)
+# - Need Maven (just `apt install maven`)
+# - Need bioformats2raw and raw2ometiff, downloaded from GitHub
+# - Those binaries (with their deps) are in my home folder
+# - I put scripts in /usr/local/bin to call the binaries
+
+# Other notes from Theo:
+# - Script takes one arg, a directory path
+# - That dir should *not* be at the bottom level, but one up
+# - Script wants a dir that contains another dir by the same name
+# - Output consists of a new dir with combined images,
+#   a zip of that same dir, and a JSON file with metadata
 
 
 def main() -> None:
